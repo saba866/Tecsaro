e410be51376012eedbfb0563725f059b4bad27ab
Saba Begum <begumsaba430@gmail.com>
2026-01-09 02:52:05 +0530

    Fix next.config.mjs dirname ESM support

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,8 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   typescript: {
-//     ignoreBuildErrors: true,
-//   },
-//   images: {
-//     unoptimized: true,
-//   },
-// }
+import path from "path";
+import { fileURLToPath } from "url";
 
-// export default nextConfig
-
-
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
